bc7121bb7761cd593174c2a2d1d57a8188524f12
John Sullivan <sulljohn12@gmail.com>
2020-02-15 18:12:34 -0500

    Add email with Sendgrid

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ module SaasProjectApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Source: https://railsapps.github.io/rails-environment-variables.html
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
